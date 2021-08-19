67a5dc7ce9282a10b78b5de621ac5ad1ef77119e
Kiichi Tachibana <bodega1123@gmail.com>
2021-08-19 20:19:08 +0900

    Add Zangief

--- src/lib/components/Zangief.tsx
+++ src/lib/components/Zangief.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import normal from '../assets/zangiefDefault.gif'
+import stand from '../assets/zangiefStand.gif'
+import walk from '../assets/zangiefWalk.gif'
+import jump from '../assets/zangiefJump.gif'
+import attack from '../assets/zangiefLightAttack.gif'
+import damaged from '../assets/zangiefDamaged.gif'
 
-const Zangief: React.FC = () => {
-  return <div>Zangief</div>
+type Props = {
+  action?: 'normal' | 'stand' | 'walk' | 'jump' | 'attack' | 'damaged'
+  size?: number
+}
+
+const ACTIONS = {
+  normal,
+  stand,
+  walk,
+  jump,
+  attack,
+  damaged
+}
+
+const Zangief: React.FC<Props> = ({ action = 'normal', size = 32 }) => {
+  return (
+    <img
+      src={ACTIONS[action]}
+      alt="Zangief"
+      width={size}
+      style={{
+        imageRendering: 'pixelated'
+      }}
+    />
+  )
 }
 
 export default Zangief
